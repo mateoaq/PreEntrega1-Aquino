42f195555357db6ce09c46be1beeb4746bc8f590
Mateo Aquino <57442760+mateoaq@users.noreply.github.com>
2022-10-20 17:33:02 -0300

    PreEntrega1+Aquino

---
+++ src/components/CartWidget/index.jsx
@@ -0,0 +1,5 @@
+import AddShoppingCartIcon from "@mui/icons-material/AddShoppingCart";
+
+export const CartWidget = () => {
+  return <AddShoppingCartIcon />;
+};
---
+++ src/components/ItemListContainer/index.jsx
@@ -0,0 +1,7 @@
+export const ItemListContainer = ({ saludo }) => {
+  return (
+    <>
+      <p>{saludo}</p>
+    </>
+  );
+};
